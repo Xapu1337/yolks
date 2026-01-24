9d1dd355fc622fd08ecc46a7442d99f675a15ebe
Parax <xapursplaz@gmail.com>
2026-01-24 16:58:38 +0100

    add a palworld proton yolk

---
+++ palworld-pterodactyl-proton/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/container
+
+# Output Current Wine Version
+wine --version
+proton --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
